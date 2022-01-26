f37c0b1ff4481d953415f2d1396e1a8bbb2625ec
stitou-nada <nadastito1087@gmail.com>
2022-01-26 12:55:56 +0000

    add

--- loop/while.php
+++ loop/while.php
@@ -1,9 +1,10 @@
 <?php 
-$x = 1 ; 
 
-while ($x <= 4) {
+$x = 1 ; //Initialiser le compteur de boucle ($x), et mettre la valeur de départ à 1
+
+while ($x <= 4) { // Continuer la boucle tant que $x est inférieur ou égal à 4
     echo "this number is : $x " ;
-    $x++;
+    $x++; //Augmenter la valeur du compteur de boucle de 1 à chaque itération
 }
 
 ?>
