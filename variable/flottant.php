57f931a2f8e4cc03a81a01fcb53f5d745f8840e8
stitou-nada <nadastito1087@gmail.com>
2022-01-26 10:37:44 +0000

    add

---
+++ variable/flottant.php
@@ -0,0 +1,13 @@
+<!-- exmple 1 -->
+<?php  
+$x = 10.365;
+var_dump($x);
+?>   
+<!-- exmple2 -->
+<?php
+   $many = 2.2888800;
+   $many_2 = 2.2111200;
+   $few = $many + $many_2;
+   
+  echo("$many + $many_2 = $few ");
+?>
