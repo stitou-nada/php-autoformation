3a1f76664c1e3948f9ffde183df35d7ce6275126
nada stitou <nadastito1087@gmail.com>
2022-02-02 21:45:19 +0100

    edit

---
+++ File Inclusion/include.php
@@ -0,0 +1,16 @@
+<?php
+include 'function.php';
+?>
+
+    <section>
+        <div>
+            <h1>hello people</h1>
+        </div>
+    </section>
+    <?php
+    include 'function.php';
+    ?>
+
+</body>
+</html>
+© 2022 GitH
