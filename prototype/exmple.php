2f7d17d07ba4f364eb8442d2e9c92caea2aa29ca
stitou-nada <nadastito1087@gmail.com>
2022-02-04 12:04:47 +0000

    add

--- prototype/exmple.php
+++ prototype/exmple.php
@@ -1,36 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+$trouve = False;
+$listNumbers = array();
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <?php
-            // declaration des variable
-            $trouve;
-            $listNumbers;
-            $userInput; 
-            // saisire
-            $listNumbers = array(1 ,10, 4, 987, 9) ;
-            $NUM= 5;
-
-            // traitment
-            for ($i = 0; $i <= 4; $i++); {
-                if ($NUM == $listNumbers[$i]) {
-                   echo $NUM ;
-                }
-            }
-            if ($trouve) {
-                window.alert("le nombre n'existe dans la position " + $i);
-            } else {
-                window.alert("le nombre n'existe pas");
-            }
-        
-    ?>
-</body>
-
-</html>
+$listNumbers[0] = 1;
+$listNumbers[1] = 10;
+$listNumbers[2] = 4;
+$listNumbers[3] = 987;
+$listNumbers[4] = 9;
+$userInput = 11;
+for ($i = 0; $i <= 4; $i++) {
+    if ($userInput == $listNumbers[$i]) {
+        $trouve = True;
+    }
+}
+if ($trouve) {
+    echo "le nombre existe" ;
+} else {
+    echo "le nombre n'existe pas " ;
+}
+?>
