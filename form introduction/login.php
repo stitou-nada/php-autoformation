3a295e3103409643c7a3d0d4758dfb56a5c6c869
stitou-nada <nadastito1087@gmail.com>
2022-02-17 09:11:51 +0000

    edit

---
+++ form introduction/login.php
@@ -0,0 +1,10 @@
+<?php
+    if(isset($_POST["username"], $_POST["password"])){
+        $username = $_POST["username"];
+
+        echo "Welcom $username to the website!";
+    } else {
+        echo "You need to enter your username and password";
+    }
+    
+?>
