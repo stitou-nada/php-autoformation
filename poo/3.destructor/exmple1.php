42f93c308851d9a4d2f30e71b7351cb8649e3de9
nada stitou <nadastito1087@gmail.com>
2022-12-28 23:20:40 +0100

    exmple desctruct

---
+++ poo/3.destructor/exmple1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+</head>
+<body>
+<?php
+class Fruit {
+  // Properties
+  var $name;
+  var $color;
+
+  // Methods
+  function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color; 
+  }
+  function __destruct() {
+    echo "The fruit is {$this->name} and the color is {$this->color}."; 
+  }
+}
+
+$apple = new Fruit("Apple", "red");
+?>
+ 
+</body>
+</html>
