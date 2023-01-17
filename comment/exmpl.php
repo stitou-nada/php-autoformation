e123aa9c3dec969afbf1e59549985ec6eab28b4a
nada stitou <nadastito1087@gmail.com>
2023-01-17 23:21:23 +0100

    comment

---
+++ comment/exmpl.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+// Vous pouvez également utiliser des commentaires pour omettre des parties d'une ligne de code
+$x = 5 /* + 15 */ + 5;
+echo $x;
+?>
+ 
+</body>
+</html>
